0ea444caa1090168e0aab5150884ff4526470c03
cjwalt <oyedelee22@gmail.com>
2024-03-15 00:49:51 -0700

    update #104: build categories, contact us component, products page

---
+++ src/components/List/List.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import "./List.scss"
+import Card from '../Card/Card'
+
+const List = () =>{ 
+
+    const data = [
+        {
+            id:1, 
+            title:"Long sleeve Graphic Tshirt",
+            img:"https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            img2:"https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            isNew:true,
+            oldPrice: 19, 
+            price:12
+        },
+        {
+            id:2, 
+            title:"Denim Jeans",
+            img:"https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            isNew:true,
+            oldPrice: 19, 
+            price:12
+        },
+        {
+            id:3, 
+            title:"Short sleeve Graphic Tshirt",
+            img:"https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            isNew:false,
+            oldPrice: 19, 
+            price:12
+        },
+        {
+            id:4, 
+            title:"Coat",
+            img:"https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            isNew:false,
+            oldPrice: 19, 
+            price:12
+        }
+    ]
+    return (
+        <div className='list'> {
+            data?.map(item =>(
+                <Card item={item} key={item.id} />
+            ))
+        }</div>
+
+
+    )
+}
+
+export default List
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,59 @@
-import React from 'react' 
+import React, { useState } from 'react' 
+import List from '../../components/List/List'
+import { useParams } from 'react-router-dom'
 
 
 const Products = () =>{
+
+    const catId = parseInt(useParams().id)
+
+    const [maxPrice, setMaxPrice] = useState(1000)
+
+
     return (
-        <div> Products </div>
+        <div className="products">
+            <div className="left">
+                <div className="filterItem">
+                    <h2>Product Categories</h2>
+                    <div className="inputItem">
+                        <input type="checkbox" name="" value={1} id="1" />
+                        <label htmlFor="1">Shoes</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" name="" value={2} id="2" />
+                        <label htmlFor="2">Skirts</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" name="" value={3} id="3" />
+                        <label htmlFor="3">Coats</label>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Filter by price</h2>
+                    <div className="inputItem">
+                        <span>0</span>
+                        <input type="range" name="" id="" min={0} max={1000} onChange={(e)=>setMaxPrice(e.target.value)} />
+                        <span>{maxPrice}</span>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Sort by</h2>
+                    <div className="inputItem">
+                        <input type="radio" name="price" id="asc" value="asc"  />
+                        <label htmlFor="asc">Price (Lowest first) </label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="radio" name="price" id="desc" value="asc"  />
+                        <label htmlFor="desc">Price (Highest first) </label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="right">
+                <img src='https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600' className='catImg'/>
+                <List />
+            </div>
+        </div>
     )
 }
 
